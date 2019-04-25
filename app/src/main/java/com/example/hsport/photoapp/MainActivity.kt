14dbddf66891b3d4dae3b089409861d02e562f69
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-25 12:26:07 +0200

    Test kotlin function

--- app/src/main/java/com/example/hsport/photoapp/MainActivity.kt
+++ app/src/main/java/com/example/hsport/photoapp/MainActivity.kt
@@ -11,6 +11,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    inline fun strLength(name : String?) : Int {
+        return name?.length ?: -1
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         val name : String = "Fred"
         var name2 : String? = null
 
-        
+
         val length2 = name2?.length ?: -1 // this will return -1 if the value is null -> No Null.PointerException
         val length3 = name2!!.length //this will evaluate the value also if it is null -> Null.PointerException
 
