bd52a54b90aff4fe4e928a5abb00c2b0b138fed9
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-25 11:27:27 +0200

    Discovering variables and null pointer exceptions

--- app/src/main/java/com/example/hsport/photoapp/MainActivity.kt
+++ app/src/main/java/com/example/hsport/photoapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toolbar
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,7 +14,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+
+
+        var userName = "Fred"
+        userName = "Sue"
+        val name : String = "Fred"
+        var name2 : String? = null
+
+        
+        val length2 = name2?.length ?: -1 // this will return -1 if the value is null -> No Null.PointerException
+        val length3 = name2!!.length //this will evaluate the value also if it is null -> Null.PointerException
+
+
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
